70fad31bbfe21539c4ea2bf5f29c9742141066ed
sarefinsag <shamimularefin100@gmail.com>
2021-11-17 16:36:32 +0600

    Magic 8 ball

---
+++ Projects/Magic 8 Ball/Main.java
@@ -0,0 +1,119 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("MAGIC 8-BALL: \n");
+
+        // one way to get random value
+        // generate value from 0 to bound. that's why + 1
+        Random random = new Random();
+        int choice = random.nextInt(20) + 1;
+
+        //another way to get random value
+        //int choice = (int) (Math.random() * 20 + 1);
+
+        if (choice == 1) {
+
+            System.out.println("It is certain.");
+
+        }
+        else if (choice == 2) {
+
+            System.out.println("It is decidedly so.");
+
+        }
+        else if (choice == 3) {
+
+            System.out.println("Without a doubt.");
+
+        }
+        else if (choice == 4) {
+
+            System.out.println("Yes - definitely.");
+
+        }
+        else if (choice == 5) {
+
+            System.out.println("You may rely on it.");
+
+        }
+        else if (choice == 6) {
+
+            System.out.println("As I see it, yes.");
+
+        }
+        else if (choice == 7) {
+
+            System.out.println("Most likely.");
+
+        }
+        else if (choice == 8) {
+
+            System.out.println("Outlook good.");
+
+        }
+        else if (choice == 9) {
+
+            System.out.println("Yes.");
+
+        }
+        else if (choice == 10) {
+
+            System.out.println("Signs point to yes.");
+
+        }
+        else if (choice == 11) {
+
+            System.out.println("Reply hazy, try again.");
+
+        }
+        else if (choice == 12) {
+
+            System.out.println("Ask again later.");
+
+        }
+        else if (choice == 13) {
+
+            System.out.println("Better not tell you now.");
+
+        }
+        else if (choice == 14) {
+
+            System.out.println("Cannot predict now.");
+
+        }
+        else if (choice == 15) {
+
+            System.out.println("Concentrate and ask again.");
+
+        }
+        else if (choice == 16) {
+
+            System.out.println("Don't count on it.");
+
+        }
+        else if (choice == 17) {
+
+            System.out.println("My reply is no.");
+
+        }
+        else if (choice == 18) {
+
+            System.out.println("My sources say no.");
+
+        }
+        else if (choice == 19) {
+
+            System.out.println("Outlook not so good.");
+
+        }
+        else {
+
+            System.out.println("Very doubtful.");
+
+        }
+    }
+}
